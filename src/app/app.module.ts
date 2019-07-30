92206f3a275932071561ccf00f2cdce01fecbb4a
synergy2411 <synergy2411@outlook.com>
2019-07-30 12:52:15 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IUser} from '../model/user';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html'
+})
+export class UsersComponent {
+  user : IUser = {
+    firstName : "Bill",
+    lastName : "Gates",
+    dob : new Date("Dec 21, 1964"),
+    income : 50000,
+    company : "Microsoft",
+    isWorking : true,
+    image : "assets/images/bill.jpg",
+    vote : 120
+  }
+
+  moreInfo(u : IUser){
+    alert(`Mr. ${u.firstName} is working with ${u.company}!`);
+  }
+}
